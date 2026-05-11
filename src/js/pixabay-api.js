bb2e4a642f5310026ab3b2406d21333f086e260b
Dima <dima.dycka.ddd@gmail.coml>
2026-05-11 21:26:51 +0300

    add image search app

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '55818365-f46e85877dddde0fe9a7f00cb';
+
+export function getImagesByQuery(query) {
+  return axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+}
